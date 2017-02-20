3116e41ca6a8c4233cb67743a95478b59c26de35
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2017-02-20 15:15:25 +0100

    base of axa-bose

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: [
+    'axa-base',
+    'airbnb',
+  ],
+
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {
@@ -14,14 +18,8 @@ module.exports = {
   ],
 
   rules: {
-    'dot-notation': 'off',
-    'max-len': ['warn', 140, 2],
-    'no-cond-assign': ['error', 'except-parens'],
-    'no-else-return': 'warn',
-    'no-use-before-define': ['error', { 'functions': false, 'classes': false }],
     'react/jsx-boolean-value': 'warn',
     'react/jsx-closing-bracket-location': ['error', 'after-props'],
     'react/prop-types': 'warn',
-    'semi': ['error', 'never'],
   }
 }
